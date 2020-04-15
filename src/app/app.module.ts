eafa5bb2e7849dd1a81152b9adaf4e285dcc57d9
Katrin Tourchenko <katrin.tourchenko@gmail.com>
2020-04-15 06:29:12 +0300

    Added and configured SSR Universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    // BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    // AppRoutingModule,
+    BrowserModule.withServerTransition({ appId: 'angular9-exp' }),
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
